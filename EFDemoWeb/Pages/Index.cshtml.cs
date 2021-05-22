32af11a7b3178b8a1b5a41be60be9b2a062bca43
LeoLopez02 <leonellopez02@gmail.com>
2021-05-22 18:08:34 -0400

    Added genereted.json file, modified Index.cshtml

--- EFDemoWeb/Pages/Index.cshtml.cs
+++ EFDemoWeb/Pages/Index.cshtml.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EFDataAccessLibrary.DataAccess;
+using EFDataAccessLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace EFDemoWeb.Pages
@@ -11,15 +14,28 @@ namespace EFDemoWeb.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly PeopleContext db;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, PeopleContext db)
         {
             _logger = logger;
+            this.db = db;
         }
 
         public void OnGet()
         {
+            LoadSampleData();
+        }
 
+        private void LoadSampleData()
+        {
+            if(db.People.Count() == 0)
+            {
+                string file = System.IO.File.ReadAllText("generated.json");
+                var people = JsonSerializer.Deserialize<List<Person>>(file);
+                db.AddRange(people);
+                db.SaveChanges();
+            }
         }
     }
 }
